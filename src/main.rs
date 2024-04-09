e2d5b03dfdcf55ef19b95b75e873fc518a620bc9
monkeysprocket <mjquinn88@outlook.com>
2024-04-09 22:11:11 +0100

    used the Threadpool to make the webserver multi-threaded

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::{
-    fs, 
-    io::{prelude::*, BufReader}, 
+    fs,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
 };
+use threadpool::Threadpool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = Threadpool::build(4).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -25,16 +29,15 @@ fn handle_connection(mut stream: TcpStream) {
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "hello.html")
-        },
+        }
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    );
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
